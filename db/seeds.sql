98118d9b5b75f47b7935fd1b0f1707460d8e03a1
Code Master <jeffbell.kc@gmail.com>
2020-03-05 00:53:32 -0600

    Working Prototype

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken and Mayo', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cuban extra Pickles', false);
